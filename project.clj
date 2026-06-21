1d760df2e61693c42fc6f8cbecafdeee65af4d2a
Savyasachi <jsavyasachi@gmail.com>
2026-06-21 15:56:34 -0700

    build: modernize deps and self-publish coordinate
    Bump dependencies onto current, security-supported releases:
    
    - BouncyCastle bcprov/bcpkix-jdk15on 1.70 -> jdk18on 1.80. The jdk15on
      line is retired and 1.70 carries CVE-2023-33201 plus the 2024 BC
      advisories; jdk18on 1.80 is the maintained successor.
    - commons-codec 1.15 -> 1.18.0, clj-http 3.12.3 -> 3.13.1.
    - Clojure 1.10.3 -> 1.12.0 (provided), with a 1.10/1.11/1.12 profile
      matrix and an "all" alias.
    
    Set the coordinate to net.clojars.savya/clj-oauth 1.5.6 (the bare
    clj-oauth group is gated; this is a maintained self-published fork),
    point :url/:scm at the fork, add clojars-token deploy repository and
    *warn-on-reflection*.
    
    BC 1.80's PemReader rejects leading whitespace, so dedent the indented
    RSA private-key literal in signature-test to a valid flush-left PEM.
    RSA-SHA1/PKCS#1 signing is deterministic, so the expected signatures
    are unchanged; the indentation was only ever tolerated by BC 1.70.

--- project.clj
+++ project.clj
@@ -1,14 +1,25 @@
-(defproject clj-oauth "1.5.6-SNAPSHOT"
-  :url "https://github.com/drone-rites/clj-oauth"
+(defproject net.clojars.savya/clj-oauth "1.5.6"
+  :url "https://github.com/jsavyasachi/clj-oauth"
   :license {:name "Simplified BSD License"
             :url "https://opensource.org/licenses/BSD-2-Clause"
             :distribution :repo}
   :description "OAuth support for Clojure"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [commons-codec/commons-codec "1.15"]
-                 [org.bouncycastle/bcprov-jdk15on "1.70"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.70"]
-                 [clj-http "3.12.3"]]
+  :scm {:name "git"
+        :url "https://github.com/jsavyasachi/clj-oauth"}
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [[commons-codec/commons-codec "1.18.0"]
+                 [org.bouncycastle/bcprov-jdk18on "1.80"]
+                 [org.bouncycastle/bcpkix-jdk18on "1.80"]
+                 [clj-http "3.13.1"]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.12.0"]]}
+             :clojure-1-10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :clojure-1-11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
+             :clojure-1-12 {:dependencies [[org.clojure/clojure "1.12.0"]]}}
+  :aliases {"all" ["with-profile" "+clojure-1-10:+clojure-1-11:+clojure-1-12"]}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :sign-releases false
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :test-selectors {:default (complement :integration)
                    :integration :integration
                    :all (constantly true)})
